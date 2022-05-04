21ff8566229e9b05ca169f3ac4f3a57fdaad5e9f
UrielJolodovsky <46960341@est.ort.edu.ar>
2022-05-04 15:50:14 -0300

    Initial commit

---
+++ Assets/CloneObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObject : MonoBehaviour
+{
+    public GameObject Cubito; 
+
+    public void CloneCube()
+    {
+        int contador = 1;
+        while (contador < 4)
+        {
+            Instantiate(Cubito);
+            contador++;
+        }
+    }
+}
